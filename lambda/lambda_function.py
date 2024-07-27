cd3ed0f767f1abc9cb0c7cf088d8c83fcb04a24a
fzjn89 <fzjn89@gmail.com>
2024-07-27 12:39:23 +0800

    updated lambda yml and lambda funtion file

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event. context) :
     return {
         'statusCode':200,
-        'body': json.dumps('Hello from our CICD github actions wofkflow vscode')
+        'body': json.dumps('Hello updated lambda rom vscode ')
     }
